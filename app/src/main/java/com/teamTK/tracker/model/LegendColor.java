9b116b098740ce9f618b914aac9effe917f6b598
eurobin1234 <eurobin1234@gamil.com>
2019-06-04 22:21:57 +0900

    Schema 작성 2

--- app/src/main/java/com/teamTK/tracker/model/LegendColor.java
+++ app/src/main/java/com/teamTK/tracker/model/LegendColor.java
@@ -1,4 +1,15 @@
 package com.teamTK.tracker.model;
 
 public class LegendColor {
+    private int color;
+    private String legendName;
+
+    public void setColor(int color) { this.color = color; }
+
+    public void setLegendName(String legendName) { this.legendName = legendName; }
+
+    public int getColor() { return color; }
+
+    public String getLegendName() { return legendName; }
+
 }
